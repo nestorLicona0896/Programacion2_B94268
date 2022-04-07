cfff8cc02b0c6542c923b4da05d254f92f110b7a
nestorLicona0896 <nestor.licona@ucr.ac.cr>
2022-04-07 13:06:38 -0600

    se añade mas funciones la clase calculadora

--- 03-PruebasUnitarias/src/calculadora.cpp
+++ 03-PruebasUnitarias/src/calculadora.cpp
@@ -6,15 +6,13 @@ int Calculadora::sumar(int val1, int val2){
     return val1 + val2;
 }
 
+int Calculadora::restar(int val1, int val2){
+    return val1 - val2;
+}
+
 int Calculadora::factorial(int val){
     if(val > 1){
         return val*factorial(val-1);
     }
-
     return 1;
-}
-
-int Calculadora::restar(int val1, int val2){
-    return val1 - val2;
-
 }
