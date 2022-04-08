6236e1e4b19d100e817214ce724fc5b7a42c7b54
nestorLicona0896 <nestor.licona@ucr.ac.cr>
2022-04-07 19:24:36 -0600

    creacion de un puntero simple

--- 04-Punteros/src/main.cpp
+++ 04-Punteros/src/main.cpp
@@ -5,7 +5,20 @@ using namespace std;
 
 int main () {
 
-    
+    //declaracion de un puntero sencillo
+
+    Calculadora* cal1;
+
+    // inicializacion del puntero de tipo Calculadora
+
+    cal1 = new Calculadora();
+
+    // acceso a un mimbro o funcion de la clase Calculadora mediante el puntero
+    int resultado = cal1->sumar(4, 4);
+
+    // los punteros se deben de liberar de la memoria (heap)
+    delete cal1;
+
     return 0;
 
 }
