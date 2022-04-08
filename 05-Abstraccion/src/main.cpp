5d3e6ab8005b137e0823748c1e75748a3d224602
nestorLicona0896 <nestor.licona@ucr.ac.cr>
2022-04-07 19:48:23 -0600

    Ejemplo inicial de abstracion sin punteros

---
+++ 05-Abstraccion/src/main.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+#include "empleado.h"
+#include "empleadoAsalariado.h"
+#include "empleadoPorHoras.h"
+
+#include <vector>
+#include <iostream>
+
+int main () {
+
+    
+
+    retunr 0;
+}
